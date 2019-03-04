9ae278353ef7357c7146beca5b4de68654b102ca
shashi-shivaraju <shashi2shivaraj@gmail.com>
2019-03-04 02:09:21 -0500

    code to test shared memory

---
+++ xv6_Unix/xv6-cuspring2019/elite.c
@@ -0,0 +1,16 @@
+#include "param.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
+
+int main(int argc, char const *argv[]) {
+  int* page2 = (int*)shmem_access(2);
+  page2[0] = 1337;
+  printf(1, "elite set page2 to %d\n", page2[0]);
+  exit();
+}
