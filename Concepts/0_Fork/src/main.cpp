1a073f0c61d01bb6542c78c0f46de22e42a54b77
shashi-shivaraju <shashi2shivaraj@gmail.com>
2020-08-30 01:45:06 -0500

    code updated with wait() call to prevent zombie process.

--- Concepts/0_Fork/src/main.cpp
+++ Concepts/0_Fork/src/main.cpp
@@ -2,22 +2,35 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 int main()
 {
-	pid_t ret = fork(); //creates a child process
-	if(ret == -1)
+	pid_t childPID = fork(); //creates a child process
+	if(childPID == -1)
 	{
-		printf("fork() call failed with %d\n",errno);
+		printf("fork() call failed with %d\n", errno);
 		perror("fork() call error\n");
 	}
-	else if(ret == 0)
+	else if(childPID == 0)
 	{
-		printf("This is the child process with id = %d\n",getpid());
+		printf("This is the child process with id = %d\n", getpid());
 	}
 	else
-	{
-		printf("This is the parent process with id = %d and its child process id = %d\n",getpid(),ret);
+	{	
+		printf("This is the parent process with id = %d and its child process id = %d\n", getpid(), childPID);
+	
+		int status = 0;
+		int ret = waitpid(childPID, &status,0);
+		if(ret == -1)
+		{
+			printf("wait() failed with %d\n", errno);
+			perror("wait() call failed\n");
+		}
+		else
+		{
+			printf("wait() called from parent returned %d with status %d\n",ret,status);
+		}
 	}
 
 	return 0;
